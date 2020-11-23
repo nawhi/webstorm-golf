38aaaec68e35dfeb5967cb5718435ce374a19e5c
nawhi <25394965+nawhi@users.noreply.github.com>
2020-11-23 18:56:10 +0000

    Clear screen when verifying

--- verify.sh
+++ verify.sh
@@ -1,11 +1,20 @@
-set -o pipefail
+#!/bin/bash
+
+function check() {
+  set -o pipefail
+  clear
+  diff -u $1 $2 |
+    yarn --silent diff-so-fancy |
+    tail -n +5 && echo 'Hole complete!' || echo 'Try again...'
+}
+
+export -f check
 
 HOLE=$1/hole.*
 TARGET=$1/target.*
 
-
-diff -u $HOLE $TARGET | yarn --silent diff-so-fancy | tail -n +5 && echo 'Hole complete!' || echo 'Try again...'
+check $HOLE $TARGET
 
 # shellcheck disable=SC2086
 fswatch -0o $HOLE $TARGET |
-  xargs -0 -n1 -I {} bash -c -o pipefail "diff -u $HOLE $TARGET | yarn --silent diff-so-fancy | tail -n +5 && echo 'Hole complete!' || echo 'Try again...'"
+  xargs -0 -n1 -I {} bash -c "check $HOLE $TARGET"
